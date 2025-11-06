5e1033d0f6f3fc607d4b94c11baeb8d1cf868de0
Gustavo Resende <gustavodcr5000@gmail.com>
2025-11-06 09:20:32 -0300

    alterações

--- gestao/models.py
+++ gestao/models.py
@@ -1,12 +1,13 @@
-# gestao/models.py
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
-# É uma boa prática usar o modelo de usuário customizado desde o início.
-# Isso nos permite adicionar campos a ele no futuro facilmente.
 class Usuario(AbstractUser):
+    class StatusAssociacao(models.TextChoices):
+        AGUARDANDO_APROVACAO = 'AGUARDANDO_APROVACAO', 'Aguardando Aprovacao'
+        APROVADO = 'PARCIALMENTE_PAGA', 'Aprovado'
+        NAO_APROVADO = 'NAO_APROVADO', 'Nao Aprovado'
+    
     apelido = models.CharField(max_length=50, blank=True, null=True)
     republica = models.ForeignKey(
         'Republica', 
@@ -15,7 +16,7 @@ class Usuario(AbstractUser):
         blank=True,
         related_name='moradores' # Nome para acessar os usuários a partir da república
     )
-    # Você pode adicionar mais campos aqui no futuro.
+    status_associacao = models.CharField(max_length=20, choices=StatusAssociacao.choices, default=StatusAssociacao.NAO_APROVADO)
 
     def __str__(self):
         return self.username
@@ -23,8 +24,8 @@ class Usuario(AbstractUser):
 class Republica(models.Model):
     nome = models.CharField(max_length=100, unique=True)
     adm = models.OneToOneField(
-        settings.AUTH_USER_MODEL, # Aponta para nosso modelo Usuario
-        on_delete=models.CASCADE, # Se o ADM for deletado, a república também é.
+        settings.AUTH_USER_MODEL, # Aponta para o modelo Usuario
+        on_delete=models.CASCADE, # Se o ADM for deletado, a república também é
         related_name='republica_administrada'
     )
 
@@ -66,10 +67,6 @@ class ParticipanteConta(models.Model):
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='participacoes')
     valor_individual = models.DecimalField(max_digits=10, decimal_places=2)
     status_pagamento = models.CharField(max_length=25, choices=StatusPagamento.choices, default=StatusPagamento.NAO_PAGO)
-
-    class Meta:
-        # Garante que um usuário não pode ser adicionado duas vezes na mesma conta
-        unique_together = ('conta', 'usuario')
 
     def __str__(self):
         return f"{self.usuario.username} na conta {self.conta.nome_conta}"
